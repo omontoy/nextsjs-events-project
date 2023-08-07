4ce6a91b273328fedcfdf27b04b73892ee759918
Orlando Montoya Benitez <orlando.montoyab@perficient.com>
2023-08-07 18:51:26 -0500

    Filter, manage and style event pages

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,8 +1,36 @@
+import EventContent from "@/components/event-detail/event-content";
+import EventLogistics from "@/components/event-detail/event-logistics";
+import EventSummary from "@/components/event-detail/event-summary";
+import ErrorAlert from "@/components/UI/error-alert";
+import { getEventById } from "@/dummy-data";
+import { useRouter } from "next/router";
+import { Fragment } from "react";
+
 const EventDetailPage = () => {
+  const router = useRouter();
+
+  const eventId = router.query.eventId;
+  const event = getEventById(eventId);
+
+  if (!event) {
+    return (
+      <ErrorAlert>
+        <p>No event found!</p>
+      </ErrorAlert>
+    );
+  }
+
   return (
-    <div>
-      <h1>Event Detail Page</h1>
-    </div>
+    <Fragment>
+      <EventSummary title={event.title} />
+      <EventLogistics
+        date={event.date}
+        address={event.address}
+        image={event.image}
+        imageAlt={event.title}
+      />
+      <EventContent>{event.description}</EventContent>
+    </Fragment>
   );
 };
 
